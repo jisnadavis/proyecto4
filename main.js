65fed9fea96b38cfe9aaedd97e6ff5a5e2c2617c
jisnadavis <jisnadavis93@gmail.com>
2024-04-20 12:37:59 +0200

    window

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ import { createheader } from './public/components/header/header'
 import { about } from './public/page/about/about'
 import './style.css'
 
+window.onload = function () {
+  document.body.style.backgroundImage = "url('./assets/sunny.jpg')"
+  document.body.style.backgroundRepeat = 'no-repeat'
+  document.body.style.backgroundSize = 'cover'
+}
+
 createheader()
 about()
 createfooter()
